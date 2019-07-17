733a5441408dd4b2bf53a2e18340ebc258082bde
jmhockham <jmhockham@gmail.com>
2019-07-17 06:52:14 +0100

    Interim commit; added highcharts example

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,13 +3,19 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import play.api.Environment
+import play.api.libs.json.Json
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(
+  cc: ControllerComponents,
+  env: Environment
+) (implicit exec: ExecutionContext) extends AbstractController(cc) {
 
   /**
    * Create an Action to render an HTML page.
@@ -20,5 +26,13 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.landingPage())
+  }
+
+  def getCounts(): Action[AnyContent] = Action.async {
+    Future {
+      val stream = env.classLoader.getResourceAsStream("")
+      val json = try {  Json.parse(stream) } finally { stream.close() }
+      Ok(json)
+    }
   }
 }
